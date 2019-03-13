fa632a604462f99eb9241871dca9d7ffe89ba553
polycarpevergete <xavier.sassi@gmail.com>
2019-03-13 15:52:01 +0700

    echec solution form_for

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,21 +2,21 @@ class FlightsController < ApplicationController
     def index
         @flights = Flight.all
         @flight = Flight.new
-        @planets = Planet.all
         @planet_options = Planet.all.map{ |p| [p.name, p.id] }
-        @duration_options = Flight.all.map{ |f| [f.duration, f.duration] }
-        @flight = Flight.new
-        @departure_flights = params[:flight][:origin_id]
-        @arrival_flights = params[:flight][:destination_id]
+        if params[:flight][:origin_id].empty? && params[:flight][:destination_id].empty?
+          @departure_flights = params[:flight][:origin_id]
+          @arrival_flights = params[:flight][:destination_id]
+        end
     end
 
     def create
-        @flight = Flight.create(params_flight)
+        @flight_search = Flight.create(params_flight)
         redirect_to root_url
     end
 
-    private 
+    private
+
     def params_flight
-      params.require(:flight).permit(:origin_id, :destination_id, :duration)
+        params.require(:flight).permit(:origin_id, :destination_id)
     end
 end
